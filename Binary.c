afba0b30413450ec577b203c43014ac60c6a0b07
Prakhar Saxena <75561866+SkyWalkerPS@users.noreply.github.com>
2021-10-10 19:47:00 +0530

    Update Binary.c

--- Binary.c
+++ Binary.c
@@ -1,65 +1,89 @@
-#include<stdio.h>
-#include"help.h"
+#include <stdio.h>
+
+int binaryproduct(int bin1, int bin2)
+{
+    int i = 0, remainder = 0, sum[20];
+    int binaryprod = 0;
+ 
+    while (bin1 != 0 || bin2 != 0)
+    {
+        sum[i++] =(bin1 % 10 + bin2 % 10 + remainder) % 2;
+        remainder =(bin1 % 10 + bin2 % 10 + remainder) / 2;
+        bin1 = bin1 / 10;
+        bin2 = bin2 / 10;
+    }
+    if (remainder != 0)
+        sum[i++] = remainder;
+    --i;
+    while (i >= 0)
+        binaryprod = binaryprod * 10 + sum[i--];
+    return binaryprod;
+}
+ 
 int main()
 {
-    while(1)
+ 
+    long bin1, bin2,multiply=0;
+   
+    int i = 0, remainder = 0, sum[20], digit, factor = 1;
+ 
+
+   
+   
+    while(1){
+    printf("Enter the Choice :\n");
+    printf("1.Addition\n2.Multiplication\n3.Exit\n");
+    int select;
+      scanf("%d",&select);
+      if(select==1){
+           printf("Enter the first binary number: ");
+    scanf("%ld", &bin1);
+    printf("Enter the second binary number: ");
+    scanf("%ld", &bin2);
+    while (bin1 != 0 || bin2 != 0)
     {
-        int choice;
-        printf("Select a choice: \n 1. Addition \n 2. Multiplication \n 3. Exit \n");
-        printf("Enter your choice: ");
-        scanf("%d",&choice);
-        if(choice==1)
+        sum[i++] =(bin1 % 10 + bin2 % 10 + remainder) % 2;
+        remainder =(bin1 % 10 + bin2 % 10 + remainder) / 2;
+        bin1 = bin1 / 10;
+        bin2 = bin2 / 10;
+    }
+    if (remainder != 0)
+        sum[i++] = remainder;
+    --i;
+    printf("Sum of two binary numbers: ");
+    while (i >= 0)
+        printf("%d", sum[i--]);
+
+      printf("\n");}
+
+      else if(select==2){
+              printf("Enter the first binary number: ");
+    scanf("%ld", &bin1);
+    printf("Enter the second binary number: ");
+    scanf("%ld", &bin2);
+    while (bin2 != 0)
+    {
+        digit =  bin2 % 10;
+        if (digit == 1)
         {
-            long binary1,binary2;
-            printf("Enter two binary no.s: ");
-            scanf("%ld %ld",&binary1,&binary2);
-            int i=0,reminder=0,sum[20];
-            while(binary1!=0 || binary2!=0)
-            {
-                sum[i++]=(binary1%10 + binary2%10 + reminder)%2;
-                reminder=(binary1%10 + binary2%10 + reminder)/2;
-                binary1=binary1/10;
-                binary2=binary2/10;
-            }
-            if(reminder!=0)
-            {
-                sum[i++]=reminder;
-                i--;
-                printf("Sum is: ");
-                while(i>=0)
-                { 
-                    printf("%d\n",sum[i--]);
-                }
-            }
-        }
-        else if(choice==2)
-        {
-            long binary1,binary2,multiply=0;
-            printf("Enter two binary no.s: ");
-            scanf("%ld %ld",&binary1,&binary2);
-            int digit,factor=1;
-            while(binary2!=0)
-            {
-                digit=binary2%10;
-                if(digit==1)
-                {
-                    binary1=binary1*factor;
-                    multiply=binaryproduct(binary1,multiply);
-                }
-                else
-                {
-                    binary1=binary1*factor;
-                    binary2=binary2/10;
-                    factor=10;
-                }
-            }
-            printf("Product is: %ld\n",multiply);
+            bin1 = bin1 * factor;
+            multiply = binaryproduct(bin1, multiply);
         }
         else
-        {
-            printf("Exiting..");
-            break;
-        }
+            bin1 = bin1 * factor;
+        bin2 = bin2 / 10;
+        factor = 10;
     }
+    printf("Product of two binary numbers: %ld", multiply);
+    printf("\n");
+
+      }
+      else if(select==3){
+          break;
+      }
+    }
+
+
+
     return 0;
 }
